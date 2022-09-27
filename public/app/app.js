b5fd525e22d997201519b3eae3de8e9b21a11179
Nischal <nischalon10@gmail.com>
2022-09-27 16:10:08 -0400

    Everything is working! Yet to Design

--- public/app/app.js
+++ public/app/app.js
@@ -1,33 +1,70 @@
+// Constants
+const baseUrl = "http://basic-web.dev.avc.web.usf.edu/";
+
+// Session Variables
+let username = sessionStorage.getItem("username");
+let url = baseUrl + username;
+let num;
+// HTML Elements
 let value = document.getElementById("value");
+document.getElementById("userID").innerText = username;
 
 
-let num = 0;
+get(url).then(function(response){
+    console.log(response);
+    if (response.status != 200){
+        post(url,{score:0}).then(function(response){
+            console.log(response);
+            num = 0;
+        });
+    }
+    num = response.data.score;
+    updateValue(num);
+});
 
-function addOne(){
-    num +=1;
+function next() {
+  num += 1;
+  fb(num);
+  post(url,{score: num});
 }
 
-function updateValue(i){
-    value.textContent = i;
+function updateValue(i) {
+  value.textContent = i;
 }
 
-function fb(num){
-    if(num % 15 == 0){
-        updateValue("FizzBuzz");
-    }
-    else if (num % 3 == 0){
-        updateValue("Fizz");
-    }
-    else if (num % 5 == 0){
-        updateValue("Buzz");
-    }
-    else{
-        updateValue(num);
-    }
+function fb(num) {
+  if (num % 15 == 0) {
+    updateValue("FizzBuzz");
+  } else if (num % 3 == 0) {
+    updateValue("Fizz");
+  } else if (num % 5 == 0) {
+    updateValue("Buzz");
+  } else {
+    updateValue(num);
+  }
 }
 
-function next(){
-    addOne();
-    fb(num);
+function get(url) {
+  return new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    http.onload = function () {
+      resolve({ status: http.status, data: JSON.parse(http.response) });
+    };
+    http.open("GET", url);
+    http.send();
+  });
 }
-nfkv
+
+function post(url, data) {
+  data = JSON.stringify(data);
+  return new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    http.onload = function () {
+      resolve({ status: http.status, data: JSON.parse(http.response) });
+    };
+    http.open("POST", url);
+    //Make sure that the server knows we're sending it json data.
+    http.setRequestHeader("Content-Type", "application/json");
+    http.send(data);
+  });
+}
--- public/main.js
+++ public/main.js
@@ -1,42 +1,13 @@
-let Url = "http://basic-web.dev.avc.web.usf.edu/";
-
 let form = document.getElementById("loginForm");
-function handleForm(e){ e.preventDefault(); }
+function handleForm(e){e.preventDefault();}
 form.addEventListener('submit',handleForm);
 
-function call(){
-  let username =  document.getElementById("username").value.trim();
-  let userUrl = Url + username;
-  get(userUrl).then(function(response){
-    console.log(response);
-    if (response.status != 200){
-      // post(Url,{score : 0})
-    }
-  });
+let username="";
+
+function login(){
+  while(username==""){
+    username = document.getElementById("username").value.trim();
+  }
+  sessionStorage.setItem("username",username);
   window.location.assign("./app/app.html");
-}
-
-
-function get(url) {
-  return new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.onload = function () {
-      resolve({ status: http.status, data: JSON.parse(http.response) });
-    };
-    http.open("GET", url);
-    http.send();
-  });
-}
-
-function post(url, data) {
-  data = JSON.stringify(data);
-  return new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.onload = function() {
-      resolve({ status: http.status, data: JSON.parse(http.response) });
-    };
-    http.open("POST", url);
-    http.setRequestHeader("Content-Type", "application/json");
-    http.send(data);
-  });
 }
